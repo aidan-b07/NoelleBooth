57a8e07334512261f72c71e3e26512beca18b44f
Aidan <aidan_booth@outlook.com>
2025-07-11 14:14:10 +0100

    Initial commit

---
+++ src/main/java/me/aidanbooth/noellebooth/NoelleBoothApplication.java
@@ -0,0 +1,13 @@
+package me.aidanbooth.noellebooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoelleBoothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoelleBoothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/aidanbooth/noellebooth/NoelleBoothApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aidanbooth.noellebooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoelleBoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
